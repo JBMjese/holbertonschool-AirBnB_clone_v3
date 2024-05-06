c4d554403b7c8416814f02dad0ea6770dc386813
JBMjese <6935@holbertonstudents.com>
2024-05-06 10:16:19 -0500

    :pencil2: implementation point 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,19 @@ and that returns a JSON indicating that the API status is "OK".
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
     data = {"status": "OK"}
     return (jsonify(data))
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Retrieve the number of each object type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+    }
+    return jsonify(stats)
