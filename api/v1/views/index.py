c37bf4b7f850f3eb89b0811e4544043745518153
JBMjese <6935@holbertonstudents.com>
2024-05-06 10:23:14 -0500

    :pencil2: fix  point 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,18 +6,26 @@ and that returns a JSON indicating that the API status is "OK".
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'])
 def status():
     data = {"status": "OK"}
     return (jsonify(data))
 
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    """Retrieve the number of each object type"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-    }
-    return jsonify(stats)
+@app_views.route('/stats')
+def clases_count():
+    '''endpoint that retrieves the number of each objects by type'''
+    new_dict = {}
+    new_dict["amenities"] = storage.count("Amenity")
+    new_dict["cities"] = storage.count("City")
+    new_dict["places"] = storage.count("Place")
+    new_dict["reviews"] = storage.count("Review")
+    new_dict["states"] = storage.count("State")
+    new_dict["users"] = storage.count("User")
+    return jsonify(new_dict)
